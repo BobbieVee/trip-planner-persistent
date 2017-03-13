cd8c70ede852f88563de9324b8f922828b0f1ed2
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-13 11:53:22 -0400

    added routes to add/remove hotel, restaurants, activities to a Day' :

--- models/index.js
+++ models/index.js
@@ -5,29 +5,32 @@ var Hotel = require('./hotel');
 var Restaurant = require('./restaurant');
 var Activity = require('./activity');
 var Day = require('./day');
-var Meal = require('./meal');
-var Adventure = require('./adventure')
+// var Meal = require('./meal');
+// var Adventure = require('./adventure')
 
 Hotel.belongsTo(Place);
 Restaurant.belongsTo(Place);
 Activity.belongsTo(Place);
 
 Day.belongsTo(Hotel);
+Day.belongsToMany(Restaurant, {through: ' meals'} );
+Day.belongsToMany(Activity, {through: 'adventures'});
 
 //Day.belongsToMany(Restaurant, {through: 'meal'});
 //Day.belongsToMany(Activity, {through: 'adventure'});
 //Restaurant.belongsToMany(Day, {through: 'meal'});
 //Activity.belongsToMany(Day, {through: 'adventure'});
 
-Meal.belongsTo(Day);
-Meal.belongsTo(Restaurant);
-Day.hasMany(Meal);
-Restaurant.hasMany(Meal);
+//Bob - commented out below. used "through" above to establish join tables:
+// Meal.belongsTo(Day);
+// Meal.belongsTo(Restaurant);
+// Day.hasMany(Meal);
+// Restaurant.hasMany(Meal);
 
 
-Adventure.belongsTo(Day);
-Adventure.belongsTo(Activity);
-Day.hasMany(Adventure);
-Activity.hasMany(Adventure);
+// Adventure.belongsTo(Day);
+// Adventure.belongsTo(Activity);
+// Day.hasMany(Adventure);
+// Activity.hasMany(Adventure);
 
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -69,5 +69,83 @@ router.get('/api/days', (req, res, next)=> {
   .catch(next);
 });
 
+router.post('/api/days', (req, res, next)=> {
+  Day.create(req.body)
+  .then((day)=>{
+    res.status(201).send(day);
+  })
+  .catch(next);
+});
+
+router.delete('/api/days/id', (req, res, next)=> {
+  Day.destroy({where: {id: req.params.id}})
+  .then(()=>{
+    res.sendStatus(204);
+  })
+  .catch(next);
+});
+
+//register a hotel to a day
+router.put('/api/days/:id/hotel', (req, res, next)=> {
+  Day.findById(req.params.id)
+  then((day)=> {
+    return day.setHotel(req.body.hotelId);
+  })
+  .then(()=> {
+    res.sendStatus(204);
+  })
+  .catch(next);
+})
+
+//register a restaurant to a day
+router.put('/api/day/:id/restaurant', (req, res, next)=>{
+  Day.findById(req.params.id)
+  .then((day)=>{
+    return day.addRestaurant(req.body.restaurantId)
+  })
+  .then(()=> res.sendStatus(204))
+  .catch(next);
+});
+
+//register an activity to a day
+router.put('/api/day/:id/activity', (req, res, next)=>{
+  Day.findById(req.params.id)
+  .then((day)=>{
+    return day.addActivity(req.body.activityId)
+  })
+  .then(()=> res.sendStatus(204))
+  .catch(next);
+});
+
+//remove a hotel from a day
+router.delete('/api/days/:id/hotel', (req, res, next)=> {
+  Day.findById(req.params.id)
+  .then((day)=> {
+    return day.setHotel(null);
+  })
+  .then(()=> res.sendStatus(204))
+  .catch(next);
+});
+
+//remove a restaurant from a day
+router.delete('/api/day/:id/restautants/:restaurantId', (req, res, next)=> {
+  Day.findById(req.params.id)
+  .then((day)=> {
+    return day.removeRestaurant(req.params.restaurantId);
+  })
+  .then(()=> res.sendStatus(204))
+  .catch(next);
+});
+
+
+//remove an activity from a day
+router.delete('/api/day/:id/activities/:activityId', (req, res, next)=> {
+  Day.findById(req.params.id)
+  .then((day)=> {
+    return day.removeActivity(req.params.activityId);
+  })
+  .then(()=> res.sendStatus(204))
+  .catch(next);
+});
 
 module.exports = router;
